2c2a9b9026214c3c330c1026a059343a1972f67c
Hilmi AM <imiia75775@proton.me>
2024-06-02 22:41:12 +0700

    add fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,28 @@
+set PATH ~/bin $PATH
+
+bind \cf tmux-sessionizer
+
+function svim --wraps nvim --description 'sudo in nvim'
+    sudo nvim -c "set noundofile" $argv
+end
+
+alias dc="docker compose"
+
+# ssh
+set -gx  SSH_KEY_PATH "~/.ssh/id_ed25519"
+
+set -gx GPG_TTY $(tty)
+set -gx TERM kitty
+set -gx EDITOR "kitty -e nvim"  # Example for using Kitty as a text editor
+set -gx FILE_MANAGER nemo  # Example for setting Nemo as default file manager
+
+function opdeps --wraps ~/bin/pacman-optional-deps.sh --description 'install pacman opdeps'
+    if test -z $argv
+        echo "No arguments specified. You must specify a package"
+        return 1
+    end
+
+    # Call the external script (replace with actual script path)
+    ~/bin/pacman-optional-deps.sh $argv
+end
+

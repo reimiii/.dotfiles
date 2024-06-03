c21867f873c453598c626fa5bc31faca79ef7359
Hilmi AM <imiia75775@proton.me>
2024-06-03 13:26:22 +0700

    add magic

--- first.sh
+++ first.sh
@@ -1,1 +1,18 @@
+#!/usr/bin/env bash
+
 mkdir -p ~/Dev/personal ~/Videos/CS/LL ~/Videos/CS/vd ~/Videos/CS/PZN
+
+sudo pacman -S $(cat pacman.txt)
+
+yay -S $(cat yay.txt)
+
+if [[ -d ~/.config/i3 ]]; then
+   mv ~/.config/i3 ~/.config/i3.defs 
+fi
+
+stow bin/ dunst/ fish/ i3/ idea/ kitty/ nvim/ rofi/ tmux/ wallpaper/
+
+sudo gpasswd -a $USER docker
+sudo gpasswd -a $USER video
+
+echo "done..."
